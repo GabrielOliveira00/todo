2c94e1a9cf93a2834398dc01351bbda3907599da
Gabriel <mgabriel.moliveira@gmail.com>
2020-12-10 21:04:03 -0300

    Adicionando compenente e alterações

---
+++ index.js
@@ -0,0 +1,30 @@
+var addTask = document.querySelector("adiciona-task");
+
+addTask.addEventListener("click", function(event)){
+
+
+  event.preventDefault();
+
+
+
+function obtemTask(task){
+
+  var task = document.querySelector("todo-items");
+   console.log("teste");
+}
+
+}
+
+function montaBotao(dado, classe){
+  var botaoTask = document.createElement("button")
+
+}
+function montaCheckBox(){
+  var checkBotao = document.createElement("input")
+  checkBotao.classList.add("check")
+}
+var conf = confirma
+
+function confirma(teste){
+  addEventListner.button
+}
